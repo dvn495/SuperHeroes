ff6ef4a289c347ac7bba71f9cbc12e5d9524cdc0
Nicolas Pedraza Maldonado <152308538+dvn495@users.noreply.github.com>
2024-02-29 22:14:18 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,286 @@
+let heroesMarvel= [
+    {
+    "id": "01",
+    "about": "Ipsum duis incididunt ullamco tempor. Amet incididunt Lorem consequat labore culpa. Pariatur amet veniam reprehenderit sunt laborum excepteur. Labore eu ut ut Lorem labore aliqua quis ex elit nulla in incididunt commodo aliquip. Velit excepteur eiusmod Lorem esse officia. Irure aliquip Lorem fugiat voluptate dolor duis consectetur aliquip pariatur tempor reprehenderit deserunt.",
+    "picture": "https://www.sideshow.com/storage/product-images/903421/iron-man_marvel_gallery_5c4cced10da7f.jpg",
+    "name": "Iron Man"
+},
+{
+    "id": "02",
+    "about": "Laboris est duis eiusmod adipisicing cillum ut sit ea Lorem non laboris quis Lorem. Est culpa esse aliqua non labore dolor esse labore nulla mollit. Nostrud amet est quis adipisicing dolor. Labore veniam elit veniam non ad ex consequat excepteur eiusmod. Minim cupidatat velit Lorem culpa quis consequat incididunt qui amet incididunt pariatur ex aliquip aliqua. Magna ex elit in aliquip minim eu ut ut fugiat ullamco deserunt adipisicing cillum.",
+    "picture": "https://static.wikia.nocookie.net/characters-of-the-pop-culture-world/images/c/c4/Bruce_Banner_%28Earth-9591%29_from_Ruins_Vol_1_1_001.jpg",
+    "name": "Hulk"
+},
+{
+    "id": "03",
+    "about": "Laboris est duis eiusmod adipisicing cillum ut sit ea Lorem non laboris quis Lorem. Est culpa esse aliqua non labore dolor esse labore nulla mollit. Nostrud amet est quis adipisicing dolor. Labore veniam elit veniam non ad ex consequat excepteur eiusmod. Minim cupidatat velit Lorem culpa quis consequat incididunt qui amet incididunt pariatur ex aliquip aliqua. Magna ex elit in aliquip minim eu ut ut fugiat ullamco deserunt adipisicing cillum.",
+    "picture": "https://i.pinimg.com/736x/ba/9b/36/ba9b3623c9a639296e85f7ff09c3c205.jpg",
+    "name": "Captain America"
+},
+{
+    "id": "04",
+    "about": "Ipsum tempor sunt Lorem est. Fugiat nisi velit veniam labore et ullamco minim adipisicing do culpa. Cillum voluptate reprehenderit aute consectetur.",
+    "picture": "http://im.rediff.com/movies/2019/mar/06captain-marvel1.jpg",
+    "name": "Captain Marvel"
+},
+{
+    "id": "05",
+    "about": "Deserunt voluptate aliquip ex dolor Lorem exercitation aliqua nisi fugiat aliquip sunt non. Eu cillum enim velit exercitation officia proident exercitation ipsum exercitation Lorem nulla do. Minim sint dolor nostrud ipsum laborum. Velit ea ad ad consectetur nisi Lorem laborum officia esse. Do eu incididunt eiusmod voluptate excepteur consequat ipsum ad. Quis veniam exercitation eiusmod amet non non eu aliquip quis ea. Tempor deserunt nulla adipisicing qui fugiat ipsum labore duis et ea consectetur.",
+    "picture": "https://m.media-amazon.com/images/M/MV5BYTU4NmRhNTctMjI2ZS00ZTUyLWI1ZjEtZWU1ODAxMjM5MzlmXkEyXkFqcGdeQXVyNDQxNjcxNQ@@._V1_UY1200_CR165,0,630,1200_AL_.jpg",
+    "name": "Black Widow"
+},
+{
+    "id": "06",
+    "about": "Esse magna Lorem ipsum incididunt sit. Enim eiusmod proident in adipisicing anim excepteur laborum magna aliqua adipisicing ad qui aliqua. Eiusmod enim ullamco laboris adipisicing tempor sint labore. Exercitation cupidatat incididunt nostrud velit nisi aute eu ex pariatur deserunt mollit. Deserunt nisi enim irure elit. Aute amet amet consequat laborum dolor sint anim Lorem id fugiat commodo cupidatat ad. Ipsum quis enim consectetur occaecat laboris esse.",
+    "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/48/Falcon_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212822",
+    "name": "Falcon"
+},
+{
+    "id": "07",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/99/Black_Panther_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212436",
+    "name": "Black Panther"
+},
+{
+    "id": "08",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://comic-watch.com/wp-content/uploads/2023/05/drs2023003_preview-page-001.jpg",
+    "name": "Doctor Strange"
+}, {
+    "id": "09",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://www.sideshow.com/storage/product-images/903735/spider-man-advanced-suit_marvel_silo.png",
+    "name": "Spider Man"
+},
+{
+    "id": "10",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://fsmedia.imgix.net/f5/ba/d8/66/7dd4/44e9/8e0a/d91348489582/ant-man-and-the-wasp-2018.jpeg#image",
+    "name": "Ant Man"
+},
+{
+    "id": "11",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Qum9HPm82yeUaFuMhpo3A98HbgsyUSDmk1cUNS4C2rb31BHZ2w",
+    "name": "The Wasp"
+},
+{
+    "id": "12",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://i.pinimg.com/originals/66/7f/b6/667fb60ce35c50cf2a36217fc65d9ff1.jpg",
+    "name": "Daredevil"
+},
+{
+    "id": "13",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://pm1.aminoapps.com/7512/185c18b92766ba8aecffa47ca4cfef9e2a67c429r1-1079-1388v2_00.jpg",
+    "name": "Ghost Rider"
+},
+{
+    "id": "14",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://i.pinimg.com/originals/88/43/fc/8843fca1fd1df67f1a27a103d30a0d33.jpg",
+    "name": "Star Lord"
+},
+{
+    "id": "15",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://i.pinimg.com/originals/ea/0c/5d/ea0c5d668838d622d04d0bdb0c31f4c2.png",
+    "name": "The One Above All"
+}]
+
+let heroesDc= [
+    {
+    "id": "01",
+    "about": "Ipsum duis incididunt ullamco tempor. Amet incididunt Lorem consequat labore culpa. Pariatur amet veniam reprehenderit sunt laborum excepteur. Labore eu ut ut Lorem labore aliqua quis ex elit nulla in incididunt commodo aliquip. Velit excepteur eiusmod Lorem esse officia. Irure aliquip Lorem fugiat voluptate dolor duis consectetur aliquip pariatur tempor reprehenderit deserunt.",
+    "picture": "https://www.planetebd.com/dynamicImages/album/cover/large/23/24/album-cover-large-23242.jpg",
+    "name": "Batman"
+},
+{
+    "id": "02",
+    "about": "Mollit officia ad excepteur anim proident incididunt eiusmod mollit consectetur id sit velit. Laborum ut magna officia qui laboris eiusmod do culpa. Culpa dolor officia velit cillum culpa deserunt cupidatat cillum ipsum laborum.",
+    "picture": "https://m.media-amazon.com/images/I/91oY-hQ+JvL._AC_UF1000,1000_QL80_.jpg",
+    "name": "Martian Manhunter"
+},
+{
+    "id": "03",
+    "about": "Laboris est duis eiusmod adipisicing cillum ut sit ea Lorem non laboris quis Lorem. Est culpa esse aliqua non labore dolor esse labore nulla mollit. Nostrud amet est quis adipisicing dolor. Labore veniam elit veniam non ad ex consequat excepteur eiusmod. Minim cupidatat velit Lorem culpa quis consequat incididunt qui amet incididunt pariatur ex aliquip aliqua. Magna ex elit in aliquip minim eu ut ut fugiat ullamco deserunt adipisicing cillum.",
+    "picture": "https://static.wikia.nocookie.net/vsdebating/images/d/dc/Justice_Society_Vol_3_10_Textless.jpg",
+    "name": "Superman"
+},
+{
+    "id": "04",
+    "about": "Ipsum tempor sunt Lorem est. Fugiat nisi velit veniam labore et ullamco minim adipisicing do culpa. Cillum voluptate reprehenderit aute consectetur.",
+    "picture": "https://www.latercera.com/resizer/dbbs_DleE-ywOqWDd0zkf5szXfg=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/VJKWYTUQXVAVHGUBDTOOTUSBYA.png",
+    "name": "Wonder Woman"
+},
+{
+    "id": "05",
+    "about": "Deserunt voluptate aliquip ex dolor Lorem exercitation aliqua nisi fugiat aliquip sunt non. Eu cillum enim velit exercitation officia proident exercitation ipsum exercitation Lorem nulla do. Minim sint dolor nostrud ipsum laborum. Velit ea ad ad consectetur nisi Lorem laborum officia esse. Do eu incididunt eiusmod voluptate excepteur consequat ipsum ad. Quis veniam exercitation eiusmod amet non non eu aliquip quis ea. Tempor deserunt nulla adipisicing qui fugiat ipsum labore duis et ea consectetur.",
+    "picture": "https://gcomics.online/wp-content/uploads/2023/07/flash-portada-flashpoint-dccomics.jpg",
+    "name": "Flash"
+},
+{
+    "id": "06",
+    "about": "Esse magna Lorem ipsum incididunt sit. Enim eiusmod proident in adipisicing anim excepteur laborum magna aliqua adipisicing ad qui aliqua. Eiusmod enim ullamco laboris adipisicing tempor sint labore. Exercitation cupidatat incididunt nostrud velit nisi aute eu ex pariatur deserunt mollit. Deserunt nisi enim irure elit. Aute amet amet consequat laborum dolor sint anim Lorem id fugiat commodo cupidatat ad. Ipsum quis enim consectetur occaecat laboris esse.",
+    "picture": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1342895794i/7052524.jpg",
+    "name": "Green Lantern"
+},
+{
+    "id": "07",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/05/Cuantas-versiones-de-Robin-han-acompanado-a-Batman-a-lo-largo-de-su-historia-3.jpg?resize=1280%2C2048&quality=80&ssl=1",
+    "name": "Robin"
+},
+{
+    "id": "08",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://www.latercera.com/resizer/Dj732kku9uNHrjK-sHLSvIlv1BM=/arc-anglerfish-arc2-prod-copesa/public/TTUSKCBHNJD7JHXTVRPSH5Y3YY.jpeg",
+    "name": "Red Hood"
+}, {
+    "id": "09",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://content.eccediciones.com/productos/19438/Nightwing_vol1_MejorQueBatman_Cubierta.jpg",
+    "name": "Nightwing"
+},
+{
+    "id": "10",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://content.eccediciones.com/productos/5400/cubierta_lucifer_ed_integral_num1_2Ed.jpg",
+    "name": "Lucifer"
+},
+{
+    "id": "11",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://content.eccediciones.com/productos/20317/UniversoSandman_Hellblazer_Cubierta_.jpg",
+    "name": "John Constantine"
+},
+{
+    "id": "12",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://pm1.aminoapps.com/6080/52b884ef7aaf17b2877f4b29e91b42825b538b0c_hq.jpg",
+    "name": "El Bromas"
+},
+{
+    "id": "13",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://content.eccediciones.com/productos/6989/PORTADA_JPG_WEB_batman_vol_4_YoSoyBane.jpg",
+    "name": "Bane"
+},
+{
+    "id": "14",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://i1.whakoom.com/large/31/32/3daf66fb144c40e69859ce0b5b96670a.jpg",
+    "name": "Reverse Flash"
+},
+{
+    "id": "15",
+    "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
+    "picture": "https://m.media-amazon.com/images/M/MV5BM2JlZTdjMjEtZjY3Zi00ZmUwLWI1NTUtODgxZjVkNDlkNWVlXkEyXkFqcGdeQXVyMTI1MTU5MjQ1._V1_.jpg",
+    "name": "The Precense"
+}]
+
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("searchInput");
+    const comicCompany = document.getElementById("comicCompany");
+    const marvelContainer = document.getElementById("marvelHeroesContainer");
+    const dcContainer = document.getElementById("DCHeroesContainer");
+
+    searchInput.addEventListener("input", handleSearch);
+    comicCompany.addEventListener("change", handleSearch);
+
+    function handleSearch() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const selectedCompany = comicCompany.value;
+        const filteredHeroes = selectedCompany === "marvel" ? heroesMarvel : heroesDc;
+
+        const filteredResults = filteredHeroes.filter(hero =>
+            hero.name.toLowerCase().includes(searchTerm)
+        );
+        marvelContainer.innerHTML = "";
+        dcContainer.innerHTML = "";
+
+        showHeroes(filteredResults);
+    }
+
+    function showHeroes(heroes) {
+        const container = comicCompany.value === 'marvel' ? marvelContainer : dcContainer;
+        
+        heroes.forEach(hero => {
+            const heroElement = createHeroElement(hero);
+            container.appendChild(heroElement);
+        });
+    }
+
+    function createHeroElement(hero) {
+        const heroElement = document.createElement("div");
+        heroElement.classList.add("containerMarvel__Heroe-box");
+
+        const imgContainer = document.createElement("div");
+        imgContainer.classList.add("containerMarvel__Heroe-img");
+        const nameContainer = document.createElement("div");
+        nameContainer.classList.add("containerMarvel__Heroe-name");
+
+        const imageElement = document.createElement("img");
+        imageElement.src = hero.picture;
+
+        const nameElement = document.createElement("h1");
+        nameElement.textContent = hero.name;
+
+        const buttonElement = document.createElement("button");
+        buttonElement.textContent = "VER";
+        buttonElement.classList.add("buttonHeroe"); 
+
+        buttonElement.addEventListener('click', () => {
+            showHeroInfo(hero);
+        });
+
+        imgContainer.appendChild(imageElement);
+        nameContainer.appendChild(nameElement);
+        nameContainer.appendChild(buttonElement);
+
+        heroElement.appendChild(imgContainer);
+        heroElement.appendChild(nameContainer);
+
+        return heroElement;
+    }
+
+    function showHeroInfo(hero) {
+        const page = document.querySelector("main")
+        const selectedHeroInfo = document.getElementById('selectedHeroInfo');
+        selectedHeroInfo.style.display = 'block';
+        selectedHeroInfo.innerHTML = `
+            <button id="buttonCerrar">X</button>
+            <div class="overlay-content">
+                <div class="overlay-img">
+                    <img src="${hero.picture}" alt="${hero.name}">
+                </div>
+                <div>
+                    <h2>${hero.name}</h2>
+                    <p>${hero.about}</p>
+                </div>
+            </div>  
+        `;
+        selectedHeroInfo.style.visibility = "visible"
+        const buttonCerrar = document.getElementById("buttonCerrar")
+        buttonCerrar.addEventListener('click', () => {
+            selectedHeroInfo.style.visibility = "hidden"
+        })
+    }
+        const marvelSection = document.getElementById("marvelSection");
+        const dcSection = document.getElementById("dcSection");
+
+        comicCompany.addEventListener("change", function() {
+            if (comicCompany.value === "marvel") {
+                marvelSection.classList.remove("hidden");
+                dcSection.classList.add("hidden");
+            } else {
+                marvelSection.classList.add("hidden");
+                dcSection.classList.remove("hidden");
+            }
+        });
+});
+
+
+
+
